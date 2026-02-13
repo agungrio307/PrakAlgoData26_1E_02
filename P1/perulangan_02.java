e9122b7c6fc1ba4d34d5cdb214acd4925929eb40
agungrio307 <agungrio307@gmail.com>
2026-02-13 15:12:06 +0700

    percobaan perulangan_02

---
+++ P1/perulangan_02.java
@@ -0,0 +1,29 @@
+package P1;
+import java.util.Scanner;
+
+public class perulangan_02 {
+    public static void main(String[] args) {
+        Scanner agung = new Scanner(System.in);
+        System.out.print("Masukkan NIM: ");
+        String nim = agung.nextLine();
+        int n = Integer.parseInt(nim.substring(nim.length() - 2));
+        if (n < 10) {
+            n = n + 10;
+        }
+        System.out.println("n = " + n);
+        System.out.print("Output: ");
+        for (int i = 1; i <= n; i++) {
+            if (i == 10 || i == 15) {
+                continue;
+            }
+            if (i % 3 == 0) {
+                System.out.print("# ");
+            } else if (i % 2 == 0) {
+                System.out.print(i + " ");
+            } else {
+                System.out.print("* ");
+            }
+        }
+        agung.close();
+    }
+}
