68a61781f0bd2f464040436e73a7354c296d92de
Agunggg <agungrio307@gmail.com>
2026-02-26 11:40:31 +0700

    Percobaan3_Jobsheet3_Modifikasi

--- P3/MataKuliah02.java
+++ P3/MataKuliah02.java
@@ -1,4 +1,5 @@
 package P3;
+import java.util.Scanner;
 
 public class MataKuliah02 {
     public String kode;
@@ -11,5 +12,23 @@ public class MataKuliah02 {
         this.nama = nama;
         this.sks = sks;
         this.jumlahJam = jumlahJam;
+    }
+
+    public MataKuliah02() {
+    }
+
+    public void tambahData() {
+        Scanner agung = new Scanner(System.in);
+
+        System.out.print("Kode       : ");
+        kode = agung.nextLine();
+        System.out.print("Nama       : ");
+        nama = agung.nextLine();
+        System.out.print("SKS        : ");
+        String dummy = agung.nextLine();
+        sks = Integer.parseInt(dummy);
+        System.out.print("Jumlah Jam : ");
+        dummy = agung.nextLine();
+        jumlahJam = Integer.parseInt(dummy);
     }
 }
