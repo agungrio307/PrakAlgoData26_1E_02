6cd9848851fba940c0579740b7902c0e8e556719
agungrio307 <agungrio307@gmail.com>
2026-03-01 16:08:54 +0700

    Tugas bikin class Dosen

---
+++ P3/Dosen02.java
@@ -0,0 +1,22 @@
+package P3;
+
+public class Dosen02 {
+    String kode;
+    String nama;
+    Boolean jenisKelamin;
+    int usia;
+    
+    public Dosen02(String kode, String nama, Boolean jenisKelamin, int usia) {
+        this.kode = kode;
+        this.nama = nama;
+        this.jenisKelamin = jenisKelamin;
+        this.usia = usia;
+    }
+
+    public void tampilData() {
+        System.out.println("Kode          : " + kode);
+        System.out.println("Nama          : " + nama);
+        System.out.println("Jenis Kelamin : " + (jenisKelamin ? "Pria" : "Wanita"));
+        System.out.println("Usia          : " + usia);
+    }
+}
