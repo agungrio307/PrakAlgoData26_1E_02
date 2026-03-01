bc1795bb83915c54d70244852836ab5a53569409
agungrio307 <agungrio307@gmail.com>
2026-03-01 16:13:47 +0700

    Tugas

--- P3/Dosen02.java
+++ P3/Dosen02.java
@@ -2,20 +2,20 @@ package P3;
 
 public class Dosen02 {
     String kode;
-    String nama;
+    String namaa;
     Boolean jenisKelamin;
     int usia;
     
     public Dosen02(String kode, String nama, Boolean jenisKelamin, int usia) {
         this.kode = kode;
-        this.nama = nama;
+        this.namaa = nama;
         this.jenisKelamin = jenisKelamin;
         this.usia = usia;
     }
 
     public void tampilData() {
         System.out.println("Kode          : " + kode);
-        System.out.println("Nama          : " + nama);
+        System.out.println("Nama          : " + namaa);
         System.out.println("Jenis Kelamin : " + (jenisKelamin ? "Pria" : "Wanita"));
         System.out.println("Usia          : " + usia);
     }
