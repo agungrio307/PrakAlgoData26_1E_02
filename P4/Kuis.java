a598834f6e8d571a0e48d22b9150f8bf06d23100
agungrio307 <agungrio307@gmail.com>
2026-03-12 09:36:02 +0700

    ganti package

---
+++ P4/Kuis.java
@@ -0,0 +1,35 @@
+package P4;
+
+public class Kuis {
+    
+    //bikin atribut
+    String namaMember;
+    int umur;;
+    int jumlahJamLatihan;
+    double biayakeanggotaan = 3000; //biaya perjam latihan
+
+    //bikin constructor
+    Kuis(String namaMember, int umur, int jumlahJamLatihan) {
+        this.namaMember = namaMember;
+        this.umur = umur;
+        this.jumlahJamLatihan = jumlahJamLatihan;
+    }
+
+    //bikin method untuk menghitung total biaya keanggotaan
+    double totalBiaya() {
+        if (jumlahJamLatihan > 15) {
+            return biayakeanggotaan * jumlahJamLatihan * 0.1;
+        } else {
+            return biayakeanggotaan * jumlahJamLatihan;
+        }
+    }
+
+    //bikin method untuk menampilkan data member
+    void printData() {
+        System.out.println("Nama Member: " + namaMember);
+        System.out.println("Umur: " + umur);
+        System.out.println("Jumlah Jam Latihan: " + jumlahJamLatihan);
+        System.out.println("Biaya Keanggotaan: " + biayakeanggotaan);
+    }
+
+}
