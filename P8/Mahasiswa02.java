48d33fc5044f33be85143b0c26f9d89eecc117a2
agungrio307 <agungrio307@gmail.com>
2026-04-22 22:15:34 +0700

    Percobaan 1_P8

---
+++ P8/Mahasiswa02.java
@@ -0,0 +1,14 @@
+package P8;
+
+public class Mahasiswa02 {
+    Mahasiswa02(String nim, String nama, String kelas) {
+        this.nama = nama;
+        this.nim = nim;
+        this.kelas = kelas;
+        nilai = -1;
+    }
+
+    void tugasDinilai(int nilai) {
+        this.nilai = nilai;
+    }
+}
