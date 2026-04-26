8607ba0a6b03f8374f65e7b560dc059b3a3c4769
agungrio307 <agungrio307@gmail.com>
2026-04-26 21:02:01 +0700

    Modifikasi Percobaan 2_P8

--- P8/StackTugasMahasiswa02.java
+++ P8/StackTugasMahasiswa02.java
@@ -71,4 +71,18 @@ public class StackTugasMahasiswa02 {
         }
         System.out.println("");
     }
+
+    public String konversiDesimalKeBiner(int nilai) {
+        StackKonversi02 stack = new StackKonversi02(32);
+        while (nilai > 0) {
+            int sisa = nilai % 2;
+            stack.push(sisa);
+            nilai = nilai / 2;
+        }
+        String biner = new String();
+        while (!stack.isEmpty()) {
+            biner += stack.pop();
+        }
+        return biner;
+    }
 }
