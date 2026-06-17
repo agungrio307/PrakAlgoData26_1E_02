92c0b62d802b7d270bc74fd41329fbf04f92ee32
agungrio307 <agungrio307@gmail.com>
2026-06-17 12:39:50 +0700

    Percobaan 3

---
+++ P15/Mahasiswa02.java
@@ -0,0 +1,21 @@
+package P15;
+
+public class Mahasiswa02 {
+    String nim;
+    String nama;
+    String notelp;
+
+    public Mahasiswa02() {
+    }
+
+    public Mahasiswa02(String nim, String nama, String notelp) {
+        this.nim = nim;
+        this.nama = nama;
+        this.notelp = notelp;
+    }
+
+    @Override
+    public String toString() {
+        return "Mahasiswa02{" + "nim=" + nim + ", nama=" + nama + ", notelp=" + notelp + '}';
+    }
+}
